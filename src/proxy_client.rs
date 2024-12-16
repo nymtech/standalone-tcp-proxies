595433d000089496268a30da471f02a85790f06b
mfahampshire <maxhampshire@pm.me>
2024-12-16 19:02:59 +0100

    remove comment

--- src/proxy_client.rs
+++ src/proxy_client.rs
@@ -32,7 +32,6 @@ struct Args {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // nym_bin_common::logging::setup_logging();
     nym_bin_common::logging::setup_tracing_logger();
     let args = Args::parse();
 
